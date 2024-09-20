ae088d0b7144e1bf8c2c5478ff96df9b90a285f6
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2024-09-20 08:51:31 +0200

    react  + vite ready for code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './assets/css/main.css'
+import './assets/css/components.css'
+import './assets/css/buttons.css'
+import './index.css' // remove later
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
