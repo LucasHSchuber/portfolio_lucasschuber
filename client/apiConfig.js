cd7b95b053b1ddf6a452b7b8c3bc2c62430a53d4
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2024-09-26 11:44:26 +0200

    upd apiConfig.js for vercel

--- client/apiConfig.js
+++ client/apiConfig.js
@@ -1,12 +1,6 @@
 // // apiConfig.js
-
-// const isProduction = process.env.NODE_ENV === 'production';
-
-// export const API_URL = isProduction
-//     ? '/' // Use your production API endpoint
-//     : 'http://localhost:5000/'; // Use your local development endpoint
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const API_URL = isProduction
-    ? '/api/' // Use the serverless function path
+    ? 'https://lucasschuberportfolio-server.vercel.app/' // Use the serverless function path
     : 'http://localhost:5000/'; // Use your local development endpoint
