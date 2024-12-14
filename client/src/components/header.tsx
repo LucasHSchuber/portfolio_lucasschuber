3aa5e60e48054ab68400aa4055d89a186c67dc94
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2024-12-14 16:37:27 +0100

    upd

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -79,7 +79,7 @@ const Header: React.FC<HeaderProps> = ({ colorfy }) => {
             </div>
           </div>
 
-          <div className='mt-4 old-portfolio'>
+          <div className='old-portfolio'>
             <h6 style={{ width: "15em" }}>Visit my old portfolio to see more design work:</h6>
             <a className='' title='https://lucasschuberportfolioold.vercel.app/#/' href="https://lucasschuberportfolioold.vercel.app/#/" target="_blank">lucasschuberportfolioold.vercel.app</a>
           </div>
